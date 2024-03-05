4579078c8e63a778c0668a96b9f0052baeafcc28
AmarinoLama <amarinolama@gmail.com>
2024-03-05 22:20:23 +0100

    feat (Sale.java) : finish class Sale

--- stockx/src/main/java/edu/badpals/stockx/item/Sale.java
+++ stockx/src/main/java/edu/badpals/stockx/item/Sale.java
@@ -1,4 +1,31 @@
 package edu.badpals.stockx.item;
 
-public class Sale {
+public class Sale implements Offer {
+
+    private String size;
+    private Integer price;
+
+    public Sale(String size, Integer price) {
+        this.size = size;
+        this.price = price;
+    }
+
+    public String size() {
+        return this.size;
+    }
+    public int value() {
+        return this.price;
+    }
+
+    public int compareTo(Offer price) {
+        return this.price.compareTo(price.value());
+    }
+
+    @Override
+    public String toString() {
+        return "Sale{" +
+                "size='" + size + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
