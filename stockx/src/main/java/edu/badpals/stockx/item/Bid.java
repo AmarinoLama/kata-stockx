a025454cbb057afaa119cb9fd20a6185c4c88311
AmarinoLama <amarinolama@gmail.com>
2024-03-05 22:20:50 +0100

    feat (Bid.java) : add class Bid

--- stockx/src/main/java/edu/badpals/stockx/item/Bid.java
+++ stockx/src/main/java/edu/badpals/stockx/item/Bid.java
@@ -1,4 +1,31 @@
 package edu.badpals.stockx.item;
 
-public class Bid {
+public class Bid implements Offer {
+
+    private String size;
+    private Integer bid;
+
+    public Bid(String size, Integer bid) {
+        this.size = size;
+        this.bid = bid;
+    }
+
+    public String size() {
+        return this.size;
+    }
+    public int value() {
+        return this.bid.intValue();
+    }
+
+    public int compareTo(Offer bid) {
+        return this.bid.compareTo(bid.value());
+    }
+
+    @Override
+    public String toString() {
+        return "Bid{" +
+                "size='" + size + '\'' +
+                ", bid=" + bid +
+                '}';
+    }
 }
