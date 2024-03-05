61ef30042fc33c394fdb0e75bbf7424c22b577a7
AmarinoLama <amarinolama@gmail.com>
2024-03-05 22:21:07 +0100

    feat (Ask.java) : add class Ask

--- stockx/src/main/java/edu/badpals/stockx/item/Ask.java
+++ stockx/src/main/java/edu/badpals/stockx/item/Ask.java
@@ -1,4 +1,30 @@
 package edu.badpals.stockx.item;
 
-public class Ask {
+public class Ask implements Offer {
+    private String size;
+    private Integer ask;
+
+    public Ask(String size, Integer ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+
+    public String size() {
+        return this.size;
+    }
+    public int value() {
+        return this.ask;
+    }
+
+    public int compareTo(Offer ask) {
+        return this.ask.compareTo(ask.value());
+    }
+
+    @Override
+    public String toString() {
+        return "Ask{" +
+                "size='" + size + '\'' +
+                ", ask=" + ask +
+                '}';
+    }
 }
