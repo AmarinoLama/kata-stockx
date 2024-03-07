637392850a456c81ad4a2021c4d25c5655fe1225
Sergio Prieto García <serprigar@gmail.com>
2024-03-07 12:50:59 +0100

    feat(criteria.java): add Criteria interface

--- stockx/src/main/java/edu/badpals/stockx/Criteria/Criteria.java
+++ stockx/src/main/java/edu/badpals/stockx/Criteria/Criteria.java
@@ -1,5 +1,10 @@
 package edu.badpals.stockx.Criteria;
 
-public interface Criteria {
+import edu.badpals.stockx.Item.Item;
+import edu.badpals.stockx.Item.Offer;
 
+import java.util.List;
+
+public interface Criteria {
+    List<Offer> checkCriteria(Item sneaker);
 }
