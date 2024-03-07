3973e0fc559649e3d3f7fd22d63bc0b8db9c6a86
Sergio Prieto García <serprigar@gmail.com>
2024-03-07 12:53:33 +0100

    feat(Bids.java): add Bids class

--- stockx/src/main/java/edu/badpals/stockx/Criteria/Bids.java
+++ stockx/src/main/java/edu/badpals/stockx/Criteria/Bids.java
@@ -1,4 +1,19 @@
 package edu.badpals.stockx.Criteria;
 
+import edu.badpals.stockx.Item.Bid;
+import edu.badpals.stockx.Item.Item;
+import edu.badpals.stockx.Item.Offer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Bids implements Criteria{
+    public Bids() {}
+    @Override
+    public List<Offer> checkCriteria(Item sneaker) {
+        return sneaker.offers()
+                .stream()
+                .filter(o -> o instanceof Bid)
+                .collect(Collectors.toList());
+    }
 }
