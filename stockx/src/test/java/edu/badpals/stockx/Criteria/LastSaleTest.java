fa5efa40ba2c993242ed26af8df4dc7768e67e21
Sergio Prieto García <serprigar@gmail.com>
2024-03-11 10:34:48 +0100

    test(/tests/criteria/*.java): add test cases

---
+++ stockx/src/test/java/edu/badpals/stockx/Criteria/LastSaleTest.java
@@ -0,0 +1,39 @@
+package edu.badpals.stockx.Criteria;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import edu.badpals.stockx.Item.Offer;
+import edu.badpals.stockx.Item.Sale;
+import edu.badpals.stockx.Item.Sneaker;
+import org.junit.Test;
+
+
+
+public class LastSaleTest {
+
+    @Test
+    public void last_sale_test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Sale sale = new Sale("10", 356);
+        sneaker.add(sale);
+        sneaker.add(new Sale("9", 352));
+        sneaker.add(new Sale("10", 404));
+        sneaker.add(new Sale("8", 372));
+
+        Criteria lastSale = new LastSale();
+        List<Offer> actualSale = lastSale.checkCriteria(sneaker);
+        assertEquals(372, actualSale.get(0).value());
+    }
+
+    @Test
+    public void last_sale_null_test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+
+        Criteria lastSale = new LastSale();
+        List<Offer> actualSale = lastSale.checkCriteria(sneaker);
+        assertTrue(actualSale.isEmpty());
+    }
+}
